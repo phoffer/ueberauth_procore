67726ff03e9fc7c0f43700cb444f56d4f592e3c5
Gary Rennie <gazler@gmail.com>
2016-12-23 10:44:37 +0000

    chore(mix.exs): add hex.pm information

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule UeberauthProcore.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -28,6 +30,21 @@ defmodule UeberauthProcore.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:ueberauth, "~> 0.4"},
-     {:oauth2, "0.6.0"}]
+     {:oauth2, "0.6.0"},
+     {:ex_doc, "~> 0.14.0", only: :dev}]
+  end
+
+  defp description do
+    """
+    An Ueberauth strategy for using Procore (OAuth) authentication.
+    """
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "LICENCE*"],
+     maintainers: ["Gary Rennie", "Gabi Zuniga"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/voicelayer/ueberauth_procore",
+              "Docs" => "https://hexdocs.pm/ueberauth_procore"}]
   end
 end
